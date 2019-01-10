6fbf5eedd3e193df3dc97f66311ca8d54b1e9df7
NoserQjh <qjhisme@gmail.com>
2019-01-10 12:06:01 +0800

    剑指offer 12

---
+++ JianzhiOffer/12.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def Power(self, base, exponent):
+        # write code here
+        return base**exponent
+
+
+def main():
+    solution = Solution()
+    result = solution.Power(4, 2)
+    print result
+
+
+if __name__ == '__main__':
+    main()
