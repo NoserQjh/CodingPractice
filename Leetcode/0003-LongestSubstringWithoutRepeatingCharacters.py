702f7837d538e56dca6e12fda02f8187cc11c96e
NoserQJH <qjhisme@gmail.com>
2019-01-25 21:25:15 +0800

    LeetCode 0003

---
+++ Leetcode/0003-LongestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,35 @@
+'''
+@Author: NoserQJH
+@LastEditors: NoserQJH
+@Date: 2019-01-25 18:35:11
+@LastEditTime: 2019-01-25 21:25:03
+@Description:Runtime: 88 ms, faster than 37.81% of Python online submissions for Longest Substring Without Repeating Characters.
+'''
+
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        start = 0
+        max_len = 0
+        now_dict = {}
+        for idx in range(len(s)):
+            if now_dict.get(s[idx], -1) >= start:
+                start = now_dict[s[idx]] + 1
+            now_dict[s[idx]] = idx
+            if idx - start + 1 > max_len:
+                max_len = idx - start + 1
+        return max_len
+
+
+def main():
+    solution = Solution()
+    result = solution.lengthOfLongestSubstring("abcabcbb")
+    print result
+
+
+if __name__ == '__main__':
+    main()
